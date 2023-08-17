c13ffbf203eb2bc06093d5f1b94ed55b3f1d0130
sunsetsobserver <adamlukawski@me.com>
2023-08-17 20:34:40 +0200

    first commit

---
+++ src/App/index.tsx
@@ -0,0 +1,14 @@
+import Flow from '../Flow';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Performative Transactions</header>
+      <Flow />
+    </div>
+  );
+}
+
+export default App;
